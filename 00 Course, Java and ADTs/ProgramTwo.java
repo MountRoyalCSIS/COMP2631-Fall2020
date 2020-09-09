03a07be45d626517444220673900fb549d106655
Jason Heard <jasonpheard@gmail.com>
2020-09-09 11:17:28 -0600

    Add notes and links for Sep 9

---
+++ 00 Course, Java and ADTs/ProgramTwo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ProgramTwo {
+    public static void main (String[] args) {
+        int x;
+        int y = 7;
+        double z;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        x = keyboard.nextInt();
+
+        y += 2;
+        z = (y / 8 * x) / 2.1;
+
+        if (z > 2.0) {
+            for (int i = 0; i < z; i++) {
+                System.out.println("x = " + x);
+                //cout << "x = " << x << endl;
+                x = x - 2;
+            }
+        } else {
+            System.out.println("Too small... :-(");
+        }
+    }
+}
--- 00 Course, Java and ADTs/links.md
+++ 00 Course, Java and ADTs/links.md
@@ -3,3 +3,8 @@ September 3, 2020:
 - Google Meet: https://meet.google.com/psc-wcmg-zoq
 - Poll: http://etc.ch/j3Pn
 - Whiteboard: https://awwapp.com/b/u68g3eke77ovi/
+
+September 9, 2020:
+
+- Google Meet: https://meet.google.com/psc-wcmg-zoq
+- Whiteboard: https://awwapp.com/b/usx8cnsest4zf/
